baef2d892e9a2bc567424fb9f59424e6c0e525d5
usagizmo <usagizmo@gmail.com>
2022-12-13 15:09:27 +0900

    feat: update meta

--- packages/tailwind-preset-base/index.js
+++ packages/tailwind-preset-base/index.js
@@ -5,7 +5,7 @@ module.exports = {
         // When not using Web fonts
         // sans: '-apple-system, blinkMacSystemFont, Helvetica, "Yu Gothic", YuGothic, "BIZ UDPGothic", Meiryo, sans-serif',
         // sans: '-apple-system, blinkMacSystemFont, Helvetica, "Hiragino Sans", "Hiragino Kaku Gothic ProN", "BIZ UDPGothic", Meiryo, sans-serif',
-        sans: 'Inter, yakuhanjp_Noto, Noto Sans CJK JP, Noto Sans JP, sans-serif',
+        sans: 'Jost, Inter, yakuhanjp_Noto, Noto Sans CJK JP, Noto Sans JP, sans-serif',
         mono: 'Source Code Pro, Noto Sans CJK JP, Noto Sans JP, monospace',
         keycode: 'Lucida Grande',
       },
