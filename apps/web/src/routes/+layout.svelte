0db4045fc9ad02185b595f146326b1f770222db9
usagizmo <usagizmo@gmail.com>
2023-01-10 23:49:56 +0900

    feat: add <GoogleAnalytics /> (#6)

--- apps/web/src/routes/+layout.svelte
+++ apps/web/src/routes/+layout.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
   import 'tailwind-preset-base/global.css';
+  import GoogleAnalytics from './GoogleAnalytics.svelte';
 </script>
+
+<GoogleAnalytics />
 
 <div class="flex h-full flex-col">
   <main class="flex-1 px-4 pb-24 pt-10 md:pb-28 md:pt-20">
