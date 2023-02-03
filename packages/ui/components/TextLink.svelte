e5740ad4c32d1ccb7d93aef5ef8365b6270cc76b
usagizmo <usagizmo@gmail.com>
2023-02-03 23:56:06 +0900

    feat: add @tailwindcss/typography / adjust styles (#13)
    * feat: add @tailwindcss/typography / adjust styles
    * fix: add eslint to tailwind-preset-base

--- packages/ui/components/TextLink.svelte
+++ packages/ui/components/TextLink.svelte
@@ -7,12 +7,12 @@
   target={blank ? '_blank' : '_self'}
   rel="noopener noreferrer"
   {href}
-  class="text-text-link hover:text-text group relative inline-flex overflow-hidden duration-200"
+  class="text-text-link hover:text-text group relative inline-flex overflow-hidden duration-150"
 >
   <span class="group-hover h-full">
     <slot />
     <span
-      class="bg-text absolute inset-x-0 bottom-0 h-px translate-x-[-102%] duration-200 group-hover:translate-x-0"
+      class="bg-text absolute inset-x-0 bottom-0.5 h-px translate-x-[-102%] duration-150 group-hover:translate-x-0"
     />
   </span>
 </a>
