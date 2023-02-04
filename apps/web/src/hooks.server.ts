b0f3ea10403e069703d1024c716ca413ecbaa45f
usagizmo <usagizmo@gmail.com>
2023-02-05 06:17:07 +0900

    fix: remove basic authentication

--- apps/web/src/hooks.server.ts
+++ apps/web/src/hooks.server.ts
@@ -1,37 +1,12 @@
 import type { Handle } from '@sveltejs/kit';
-import { PUBLIC_OBSIDIAN_PUBLIC_AUTH_DIRS } from '$env/static/public';
-import { paramsPathToRoutePath, parseSession } from '$lib/utils';
 import { setSession } from '$houdini';
+import { parseSession } from '$lib/utils';
 import { NHOST_SESSION_KEY } from '$lib/const';
-import type { NhostSession } from '$lib/nhost';
 
 export const handle: Handle = async ({ event, resolve }) => {
   const session = parseSession(event.cookies.get(NHOST_SESSION_KEY));
   const token = session?.accessToken ?? null;
   setSession(event, { token });
-
-  const pathAuthList = PUBLIC_OBSIDIAN_PUBLIC_AUTH_DIRS.split(',').map((authDir) => {
-    const [paramsPath, userPass] = authDir.split('=');
-    const encoded = Buffer.from(userPass).toString('base64');
-    return {
-      path: paramsPathToRoutePath(paramsPath),
-      auth: `Basic ${encoded}`,
-    };
-  });
-
-  for (const { path, auth } of pathAuthList) {
-    if (event.url.pathname.startsWith(path)) {
-      const headerAuth = event.request.headers.get('Authorization');
-      if (headerAuth !== auth) {
-        return new Response('Not authorized', {
-          status: 401,
-          headers: {
-            'WWW-Authenticate': 'Basic realm="User Visible Realm", charset="UTF-8"',
-          },
-        });
-      }
-    }
-  }
 
   const response = await resolve(event);
   return response;
